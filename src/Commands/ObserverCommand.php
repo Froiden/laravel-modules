59f7ac0e4c6cffa187621c98dad67dd442234e2e
Ajay Kumar Choudhary <ajay@froiden.com>
2017-02-09 11:26:24 +0530

    Feat: initial commit

---
+++ src/Commands/ObserverCommand.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Nwidart\Modules\Commands;
+
+use Nwidart\Modules\Support\Stub;
+use Nwidart\Modules\Traits\ModuleCommandTrait;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+class observerCommand extends GeneratorCommand
+{
+    use ModuleCommandTrait;
+
+    /**
+     * The name of argument being used.
+     *
+     * @var string
+     */
+    protected $argumentName = 'observer';
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'module:make-observer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate new restful controller for the specified module.';
+
+    /**
+     * Get controller name.
+     *
+     * @return string
+     */
+    public function getDestinationFilePath()
+    {
+        $path = $this->laravel['modules']->getModulePath($this->getModuleName());
+
+        $controllerPath = $this->laravel['modules']->config('paths.generator.observer');
+
+        return $path . $controllerPath . '/' . $this->getControllerName() . '.php';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTemplateContents()
+    {
+        $module = $this->laravel['modules']->findOrFail($this->getModuleName());
+
+        return (new Stub($this->getStubName(), [
+            'MODULENAME'        => $module->getStudlyName(),
+            'CONTROLLERNAME'    => $this->getControllerName(),
+            'NAMESPACE'         => $module->getStudlyName(),
+            'CLASS_NAMESPACE'   => $this->getClassNamespace($module),
+            'CLASS'             => $this->getControllerName(),
+            'LOWER_NAME'        => $module->getLowerName(),
+            'MODULE'            => $this->getModuleName(),
+            'NAME'              => $this->getModuleName(),
+            'STUDLY_NAME'       => $module->getStudlyName(),
+            'MODEL'             => $this->getModelName(),
+            'MODULE_NAMESPACE'  => $this->laravel['modules']->config('namespace'),
+        ]))->render();
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array(
+            array('observer', InputArgument::REQUIRED, 'The name of the controller class.'),
+            array('module', InputArgument::OPTIONAL, 'The name of module will be used.'),
+        );
+    }
+
+    /**
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['plain', 'p', InputOption::VALUE_NONE, 'Generate a plain controller', null],
+        ];
+    }
+
+    protected function getModelName()
+    {
+        $controller = str_replace('Observer','',$this->getControllerName());
+
+        return $controller;
+    }
+    /**
+     * @return array|string
+     */
+    protected function getControllerName()
+    {
+        $controller = studly_case($this->argument('observer'));
+
+        if (str_contains(strtolower($controller), 'observer') === false) {
+            $controller .= 'Observer';
+        }
+
+        return $controller;
+    }
+
+
+    /**
+     * Get default namespace.
+     *
+     * @return string
+     */
+    public function getDefaultNamespace()
+    {
+        return 'Http\Controllers';
+    }
+
+    /**
+     * Get the stub file name based on the plain option
+     * @return string
+     */
+    private function getStubName()
+    {
+        return '/observer.stub';
+    }
+}
